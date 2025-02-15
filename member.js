e9d76d6254a60eb2ebdad1ad6a3a4a109c351e8a
Yumer <yumeradem.yumer@inter.ikea.com>
2025-02-15 15:45:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var member = {
+    name: 'John',
+    skills: ['JavaScript', 'React', 'Node'],
+    details: {
+      age: 25,
+      country: 'US'
+    }
+  };
+  return member;
+}
